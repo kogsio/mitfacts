73c341382f1e34ff4f1363d047a2b154412c9a6b
J. Ben Anderson <jbenanderson@Bens-iMac.local>
2021-02-18 15:11:51 -0500

    fixed date, color, and founded date.

--- mit.js
+++ mit.js
@@ -1,7 +1,7 @@
 var mit = {
-    city: 'Cambridge',
-    colors: ['pink', 'red'],
-    mascot: "robot",
-    founded: 1900,
-    motto: 'Carpe diem',
+  city: "Cambridge",
+  colors: ["Silver Gray"],
+  mascot: "robot",
+  founded: 1861,
+  motto: "Mens",
 };
