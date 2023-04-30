d134c83e2fc665e328b3f4adeebadf5f5e012ebf
George <geolcampbellv@gmail.com>
2023-04-30 14:40:01 -0400

    fixed all

--- mit.js
+++ mit.js
@@ -1,7 +1,7 @@
 var mit = {
     city: 'Cambridge',
-    colors: ['pink', 'red'],
+    colors: ['Silver Gray'],
     mascot: "robot",
     founded: 1861,
-    motto: 'Carpe diem',
+    motto: 'Mens',
 };
